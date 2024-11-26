634bf54883282865562ec9fba4432be6e12319b0
Arsalaan Khan <arslaan.tech@gmail.com>
2024-11-26 18:54:27 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-// import routes from './routes';
 
 const app = express();
 
 const port = 3000;
 app.use(express.json());
-// app.use(routes);
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
